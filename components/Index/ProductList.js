8b1639cade5d2903b91eeac3c0da3a689cf2b815
github_username <azizkoura@hotmail.com>
2019-11-19 15:36:45 +0100

    build product-card responsive

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -1,5 +1,18 @@
-function ProductList() {
-  return <>ProductList</>;
+import { Card } from 'semantic-ui-react'
+
+function ProductList({ products }) {
+  const mapProductsToItems = (products) => {
+    return products.map(product => ({
+      header: product.name,
+      image: product.mediaUrl,
+      color: 'teal',
+      fluid: true,
+      childKey: product._id,
+      href: `/product?_id=${product._id}`
+    }))
+  }
+
+  return <Card.Group items={mapProductsToItems(products)}/>;
 }
 
 export default ProductList;
