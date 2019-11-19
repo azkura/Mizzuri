de07bcda9c264116bb45e0f9e854cfc8904eaa2e
github_username <azizkoura@hotmail.com>
2019-11-19 21:37:14 +0100

    handle input form

--- pages/create.js
+++ pages/create.js
@@ -9,13 +9,24 @@ import { Form,
   Header,
   Icon } from 'semantic-ui-react'
 
-function CreateProduct() {
+const  CreateProduct = () => {
   const [product, setProduct] = React.useState({
     name: "",
     price: "",
-    media: '',
+    media: "",
     description: ""
   })
+
+  const handleChange = (event) => {
+    const {name, value, files} = event.target
+    if(name === 'media') {
+      setProduct(prevState => ({...prevState, media: files[0] }))
+    } else {
+      setProduct(prevState => ({ ...prevState, [name]: value }))
+      event.preventDefault()
+    }
+    console.log(product)
+  }
 
   return (
     <>
@@ -25,11 +36,38 @@ function CreateProduct() {
       </Header>
       <Form >
         <Form.Group widths='equal'>
-          <Form.Input  name="name" label='Name' placeholder='Name' type="text"/>
-          <Form.Input  name="price" type="number" label='Price' placeholder='Price' min="0.00" step="0.01" />
-          <Form.Input  name="media" type="file" label="Media" accept="image/*" content="Select Image"/>       
+          <Form.Input 
+            name="name" 
+            label='Name'
+            placeholder='Name'
+            type="text"
+            onChange={handleChange}
+          />
+          <Form.Input
+            name="price"
+            type="number"
+            label='Price'
+            placeholder='Price'
+            min="0.00"
+            step="0.01"
+            onChange={handleChange}
+          />
+          <Form.Input
+            name="media"
+            type="file"
+            label="Media"
+            accept="image/*"
+            content="Select Image"
+            onChange={handleChange}
+          />       
         </Form.Group>
-        <Form.TextArea  name="description" label='Description' placeholder='Description' />
+        <Form.TextArea
+          name="description" 
+          label='Description'
+          placeholder='Description'
+          type="text"
+          onChange={handleChange}
+        />
         <Form.Button type="submit" color="blue">Submit</Form.Button>
       </Form>
     </>
