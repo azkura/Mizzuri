1ca3fee1686ef15b229d99bfa569d673b219f99e
github_username <azizkoura@hotmail.com>
2019-11-20 19:43:26 +0100

    structure cart page

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,5 +1,30 @@
+import { Header, Segment, Button, Icon } from 'semantic-ui-react'
+
 function CartItemList() {
-  return <>CartItemList</>;
+  const user = false
+  return (
+    <Segment 
+      secondary 
+      color="black" 
+      inverted 
+      textAlign="center" 
+      placeholder
+    >
+      <Header icon>
+        <Icon name="shopping basket"/>
+        No Products in your cart. Add some!
+      </Header>
+      <div>
+        { user ? (
+          <Button color="orange">View products</Button>
+          )
+          : (
+          <Button color="blue">Login to add products</Button>
+          )
+        }
+      </div>
+    </Segment>
+  )
 }
 
 export default CartItemList;
