68182c3d8a5d17319f9c9c15d4a3cd345b67cf96
github_username <azizkoura@hotmail.com>
2019-11-22 09:42:55 +0100

    validate post content on server

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,30 +2,40 @@ import connectDb from '../../utils/connectDb'
 import User from '../../models/User'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import isEmail from 'validator/lib/isEmail'
+import isLength from 'validator/lib/isLength'
 
 connectDb()
 
 export default async (req, res) => {
   const { name, email, password } = req.body
   try {
-    // 1) check to see if the user already exist in DB
+    // 1) validate name / email / password
+    if(!isLength(name, { min: 3, max: 10 })) {
+      return res.status(422).send("Name must be 3-10 characters long")
+    } else if (!isLength(password, { min: 6 })) {
+      return res.status(422).send("Password must be at least 6 characters")
+    } else if (!isLength(email)) {
+      return res.status(422).send("email must be valid")
+    }
+    // 2) check to see if the user already exist in DB
     const user = await User.findOne({ email })
     if(user) {
       return res.status(422).send(`user already exists with ${email}`)
     }
-    // 2) --if not, hash their password
+    // 3) --if not, hash their password
     const hash = await bcrypt.hash(password, 10)
-    // 3) create user
+    // 4) create user
     const newUser = await new User({
       name,
       email,
       password: hash
     }).save()
     console.log(newUser)
-    // 4) create a token for the new user
+    // 5) create a token for the new user
     const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
     expiresIn: '7d' })
-    // 5) send back token
+    // 6) send back token
     res.status(201).json(token)
   } catch (error) {
     console.log(error)
