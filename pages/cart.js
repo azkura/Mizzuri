8538d2d7521499e4b672cc8a96b6fee5f83f479b
github_username <azizkoura@hotmail.com>
2019-11-23 15:13:34 +0100

    fetch user cart from client

--- pages/cart.js
+++ pages/cart.js
@@ -1,6 +1,9 @@
 import { Segment } from 'semantic-ui-react'
 import CartItemList from '../components/Cart/CartItemList'
 import CartSummary from '../components/Cart/CartSummary'
+import { parseCoockies } from 'nookies'
+import axios from 'axios'
+import baseUrl from '../utils/baseUrl'
 
 function Cart() {
   return (
@@ -9,6 +12,16 @@ function Cart() {
       <CartSummary />
     </Segment>
   )
+}
+
+Cart.getInitialProps = async ctx => {
+  const { token } = parseCoockies(ctx)
+  if (!token) {
+    return { products: [] }
+  }
+  const url = `${baseUrl}/api/cart`
+  const payload = { headers: { Authorization: token }}
+  await axios.get(url, payload)
 }
 
 export default Cart;
