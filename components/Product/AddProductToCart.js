d6e5e4ab5aa5cd67987502ff86493b1eba437e19
github_username <azizkoura@hotmail.com>
2019-11-23 18:31:30 +0100

    fixing bugs

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -4,18 +4,30 @@ import { Input } from 'semantic-ui-react'
 
 import axios from 'axios'
 import baseUrl from '../../utils/baseUrl'
+import { catchErrors } from '../../utils/catchErrors'
 import cookie from 'js-cookie'
 
 function AddProductToCart({ user, productId }) {
   const [ quantity, setQuantity ] = useState(1)
+  const [ loading, setLoading ] = useState(false)
+  const [ success, setSuccess ] = useState(false)
   const router = useRouter()
 
   const handleAddProductToCart = async () => {
-    const url = `${baseUrl}/api/cart`
-    const payload = { quantity, productId }
-    const token = cookie.get('token')
-    const headers = { headers: { Authorization: token } }
-    const response = await axios.put(url, payload, headers)
+    try {
+      setLoading(true)
+      const url = `${baseUrl}/api/cart`
+      const payload = { quantity, productId }
+      const token = cookie.get('token')
+      const headers = { headers: { Authorization: token } }
+      await axios.put(url, payload, headers)
+      setSuccess(true)
+
+    } catch (error) {
+      catchErrors(error, window.alert)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return <Input
@@ -24,17 +36,30 @@ function AddProductToCart({ user, productId }) {
     placeholder="Quantity"
     value={quantity}
     onChange={event => setQuantity(Number(event.target.value))}
-    action={ user ? {
-      color: "orange",
-      content: "Add to Cart",
-      icon: "plus cart",
-      onclick: handleAddProductToCart
-    } : {
-      color: "blue",
-      content: "Signup to Purchase",
-      icon: "signup",
-      onclick: () => router.push("/signup")
-    }}
+    action={
+      user && success 
+        ? {
+            color: "blue",
+            content: "Item Added!",
+            icon: "plus cart",
+            disabled: true
+          } 
+      : user 
+        ? {
+            color: "orange",
+            content: "Add to Cart",
+            icon: "plus cart",
+            loading,
+            disabled: loading,
+            onClick: handleAddProductToCart
+          } 
+      : {
+          color: "blue",
+          content: "Signup to Purchase",
+          icon: "signup",
+          onClick: () => router.push('/signup')
+        }
+    }
   />
 }
 
--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,81 +1,79 @@
-import mongoose from 'mongoose'
-import jwt from 'jsonwebtoken'
-import Cart from '../../models/Cart'
-import connectDb from '../../utils/connectDb'
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import Cart from "../../models/Cart";
+import connectDb from "../../utils/connectDb";
 
-connectDb()
+connectDb();
 
-const { ObjectId } = mongoose.Types
+const { ObjectId } = mongoose.Types;
 
 export default async (req, res) => {
   switch (req.method) {
     case "GET":
-      await handleGetRequest(req, res)
-      break
+      await handleGetRequest(req, res);
+      break;
     case "PUT":
-      await handlePutRequest(req, res)
-      break
+      await handlePutRequest(req, res);
+      break;
     default:
-      res.status(405).send(`Method ${req.method} not allowed`)
-      break
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
   }
-}
+};
 
-const handleGetRequest = async (req, res) => {
-  if(!("Authorization" in req.headers)) {
-    return res.status(401).send("No authorization token")
+async function handleGetRequest(req, res) {
+  if (!("authorization" in req.headers)) {
+    return res.status(401).send("No authorization token");
   }
   try {
-    const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET)
+    const { userId } = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
     const cart = await Cart.findOne({ user: userId }).populate({
       path: "products.product",
       model: "Product"
-    })
-    res.status(200).json(cart.products)
+    });
+    res.status(200).json(cart.products);
   } catch (error) {
-    console.error(error)
-    res.status(403).send("Please login again")
+    console.error(error);
+    res.status(403).send("Please login again");
   }
 }
 
-const handlePutRequest = async (req, res) => {
-  const { quantity, productId } = req.body
-
-  if(!("Authorization" in req.headers)) {
-    return res.status(401).send("No authorization token")
+async function handlePutRequest(req, res) {
+  const { quantity, productId } = req.body;
+  if (!("authorization" in req.headers)) {
+    return res.status(401).send("No authorization token");
   }
-
   try {
     const { userId } = jwt.verify(
-      req.headers.authorization, process.env.JWT_SECRET
-    )
-
-    // 1) Get user cart based on userId
-    const cart = await Cart.findOne({ user: userId })
-
-    // 2) check if product already exists in cart
-    const productExists = cart.products.some(doc => ObjectId(productId).equals
-    (doc.product))
-
-    // 3) --if so, increment quantity (by number provided to request)
-    if(productExists) {
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    // Get user cart based on userId
+    const cart = await Cart.findOne({ user: userId });
+    // Check if product already exists in cart
+    const productExists = cart.products.some(doc =>
+      ObjectId(productId).equals(doc.product)
+    );
+    // If so, increment quantity (by number provided to request)
+    if (productExists) {
       await Cart.findOneAndUpdate(
         { _id: cart._id, "products.product": productId },
         { $inc: { "products.$.quantity": quantity } }
-      )
+      );
     } else {
-      // 4) --if not, add new product with given quantity
-      const newProduct = { quantity, product: product}
+      // If not, add new product with given quantity
+      const newProduct = { quantity, product: productId };
       await Cart.findOneAndUpdate(
         { _id: cart._id },
         { $addToSet: { products: newProduct } }
-      )
+      );
     }
-
-    res.status(200).send("Cart updated")
-
+    res.status(200).send("Cart updated");
   } catch (error) {
-    console.error(error)
-    res.status(403).send("Please login again")
+    console.error(error);
+    res.status(403).send("Please login again");
   }
 }
