bef7f508327a8e280676531b0124d187cd246619
github_username <azizkoura@hotmail.com>
2019-11-25 13:17:07 +0100

    cart checkout handling

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import StripeCheckout from 'react-stripe-checkout'
 import { Button, Segment, Divider } from 'semantic-ui-react'
 
 import { calculateCartTotal } from '../../utils/calculateCartTotal'
 
-function CartSummary({ products }) {
+function CartSummary({ products, handleCheckout, success }) {
+
   const [ isCartEmpty, setCartEmpty ] = useState(false)
   const [ cartAmount, setCartAmount ] = useState(0)
   const [ stripeAmount, setStripeAmount ] = useState(0)
@@ -18,14 +20,26 @@ function CartSummary({ products }) {
   return <>
     <Divider />
     <Segment clearing size="large">
-<strong>Sub total:</strong> { cartAmount }€
-      <Button 
-        icon="cart"
-        color="teal"
-        floated="right"
-        content="checkout"
-        disabled={isCartEmpty}
-      />
+      <strong>Sub total:</strong> { cartAmount }€
+      <StripeCheckout
+        name="Mizzuri"
+        amount={stripeAmount}
+        image={products.length > 0 ? products[0].product.mediaUrl : ""}
+        currency="eur"
+        shippingAddress="true"
+        billingAddress="true"
+        zipCode="true"
+        token={handleCheckout}
+        triggerEvent="onClick"
+      >
+        <Button 
+          icon="cart"
+          color="teal"
+          floated="right"
+          content="checkout"
+          disabled={isCartEmpty || success}
+        />
+      </StripeCheckout>
     </Segment>
   </>;
 }
