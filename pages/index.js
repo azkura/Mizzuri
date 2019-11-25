e2828de203067d3c002345cb9893955c36823c21
github_username <azizkoura@hotmail.com>
2019-11-25 17:02:58 +0100

    handling pagination from client

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,18 @@ function Home({ products }) {
   return <ProductList products={products} />;
 }
 
-Home.getInitialProps = async () => {
+Home.getInitialProps = async ctx => {
+  // Pagination
+  const page = ctx.query.page ? ctx.query.page : "1"
+  const size = 9
   // fetch data on a server
   const url = `${baseUrl}/api/products`
-  const response = await axios.get(url)
-  return { products: response.data }
+  const payload = { params: { page, size } }
+
+  const response = await axios.get(url, payload)
   // return response data as a object
+  return { products: response.data }
+
   // this object will be merge with existing props
 }
 
