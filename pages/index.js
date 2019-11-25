6918b344b8577701d3f65d138afac234ed0f753b
github_username <azizkoura@hotmail.com>
2019-11-25 18:06:49 +0100

    finishing pagination from client

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,29 @@ import axios from 'axios'
 import baseUrl from '../utils/baseUrl'
 
 import ProductList from '../components/Index/ProductList'
+import ProductPagination from '../components/Index/ProductPagination'
 
-function Home({ products }) {
-  return <ProductList products={products} />;
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  )
 }
 
 Home.getInitialProps = async ctx => {
   // Pagination
   const page = ctx.query.page ? ctx.query.page : "1"
   const size = 9
+
   // fetch data on a server
   const url = `${baseUrl}/api/products`
   const payload = { params: { page, size } }
 
   const response = await axios.get(url, payload)
   // return response data as a object
-  return { products: response.data }
+  return response.data 
 
   // this object will be merge with existing props
 }
