2bdd6ce40c97291b7817f4498cca6bf77cafa5bf
github_username <azizkoura@hotmail.com>
2019-11-26 12:35:16 +0100

    cleaning app

--- pages/product.js
+++ pages/product.js
@@ -14,7 +14,6 @@ function Product({ product, user }) {
 }
 
 Product.getInitialProps = async ({ query: { _id } }) => {
-  // console.log(query)
   const url = `${baseUrl}/api/product`
   const payload = { params: {_id} }
   const response = await axios.get(url, payload)
